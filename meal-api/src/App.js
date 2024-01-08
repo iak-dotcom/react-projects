04214a21dfd898f296e2a95fd066be1c8baddf53
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-01-08 21:56:35 +0000

    starting meal-api project

---
+++ meal-api/src/App.js
@@ -0,0 +1,9 @@
+// This file has to be as it is!
+import Main from "./Main";
+
+const App = () => {
+  return <Main/>; //If <Main/> in next line it won`t work!
+
+};
+
+export default App;
---
+++ meal-api/src/Main.js
@@ -0,0 +1,14 @@
+import react from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./style.css";
+
+function Main(){
+    return(
+        <>
+        <h1>HI</h1>
+        </>
+
+    );
+}
+export default Main;
