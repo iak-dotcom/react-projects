ac3fbfd4cb8ebaaa7f5a08bb5180f4a40d27cd3f
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-01-10 20:26:47 +0000

    Starting Calculator project

---
+++ calculator/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Counter from './Calculator';
+const App = () => {
+  return (
+<>
+<Calculator/>
+</>
+  );
+};
+
+export default App
---
+++ calculator/src/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useEffect, useState} from react; 
+import "./style.css";
+
+function Calculator(){
+  return (
+    <>
+    
+    </>
+  )
+  }
+
+export default Calculator
