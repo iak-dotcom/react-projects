cc174dced2db06e547ea060323adf9e2aa5e7375
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-01-16 23:51:21 +0000

    accordion project completed

--- accordions/src/App.js
+++ accordions/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import Accordions from './Accordions';
+import React from "react";
+import Accordions from "./Accordions";
+import { accordionData } from "./utils/content";
 const App = () => {
   return (
-<>
-<Accordions/>
-</>
+    <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => {
+          return (
+            <Accordions
+              title={title}
+              content={content}
+              key={Math.random() * 100}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
-export default App
+export default App;
