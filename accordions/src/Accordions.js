5720897f3111b35920b534d7342689e5e6b91423
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-01-16 23:17:45 +0000

    starting Accordions Project

---
+++ accordions/src/Accordions.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+import "./style.css";
+
+function Accordions() {
+ 
+
+  return (
+    <>
+   <h1> Starting Project!</h1>
+   
+    </>
+  );
+}
+
+export default Accordions;
---
+++ accordions/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Accordions from './Accordions';
+const App = () => {
+  return (
+<>
+<Accordions/>
+</>
+  );
+};
+
+export default App
