0cc78f96751befcbf721fbbe0d02698d321eabee
Imran Khan <imranahmedkhan@hotmail.co.uk>
2024-01-16 22:33:28 +0000

    Testimonials Project Finished

--- testimonials/src/Testimonials.js
+++ testimonials/src/Testimonials.js
@@ -3,10 +3,44 @@ import { useState } from "react";
 import "./style.css";
 
 function Testimonials() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const testimonials = [
+    {
+      quote: "I highly recommend it!",
+      author: "John Smith",
+    },
+    {
+      quote: "This is the best product I`ve ever used.",
+      author: "Jane Doe",
+    },
+    {
+      quote: "This was life changing experience!",
+      author: "Imran Khan",
+    },
+  ];
+
+  const handlePrevClick = () => {
+    setCurrentIndex(
+      (currentIndex + testimonials.length - 1) % testimonials.length
+    );
+  };
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % testimonials.length);
+  };
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="testimonials">
+      <div className="testimonials-quote">
+        {testimonials[currentIndex].quote}
+      </div>
+      <div className="testimonials-author">
+        - {testimonials[currentIndex].author}
+      </div>
+      <testimonials className="nav">
+        <button onClick={handlePrevClick}>Prev</button>
+        <button onClick={handleNextClick}>Next</button>
+      </testimonials>
+    </div> /* end of testimonial */
   );
 }
 export default Testimonials;
